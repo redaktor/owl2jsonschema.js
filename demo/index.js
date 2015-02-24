a4b557e84f5c58be0c65f8eae0fde00122194d10
redaktor <api@redaktorcms.com>
2015-02-24 12:32:11 +0100

    updated demo

--- demo/index.js
+++ demo/index.js
@@ -1,16 +1,20 @@
-var parseOWL = require('../index.js'); 
+var owl2jsonschema = require('../index.js');
 /* becomes require('owl2jsonschema') */
-
+var parser = new owl2jsonschema(/*[options object]*/);
 /*
 //	You can parse remote or local, absolute or relative files or an xml string :
 //	NOTE - currently there must be an RDF Container and an OWL Ontology present ...
 */
-
 // CREATES files containing the JSON Schemas in a folder OUTPUT/[host]_[path]
 // RETURNS in [callback function] a string containing the JSON structure by default
-parseOWL('http://mappings.dbpedia.org/server/ontology/export' /*, [options object], [callback function]*/ );
 
+parser.parse('http://mappings.dbpedia.org/server/ontology/export', function(){ 
+	console.log( 'FINAL CALLBACK fn says READY. ;)' ); 
+} );
+// EXAMPLES :
+// parser.parse('http://purl.org/NET/cidoc-crm/core');
+// parser.parse('http://www.ontologydesignpatterns.org/ont/dul/DUL.owl');
 // OR
-// parseOWL('./export.xml' /*, [options object], [callback function]*/);
+// parser.parse('./export.xml' /*, [callback function]*/);
 // OR
-// parseOWL('<?xml><rdf:RDF><owl:Ontology>...</owl:Ontology>...</rdf:RDF>' /*, [options object], [callback function]*/);
+// parser.parse('<?xml><rdf:RDF><owl:Ontology>...</owl:Ontology>...</rdf:RDF>' /*, [callback function]*/);
